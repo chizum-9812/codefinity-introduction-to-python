65d96b1b6b2c64fee3c51595dd5802ec0001d270
chizum-9812 <>
2026-08-13 13:07:57 +0000

    Arithmetic Operators

--- getting_started/arithmetic_operators/main.py
+++ getting_started/arithmetic_operators/main.py
@@ -2,4 +2,4 @@
 print (5.42 + 4.39)
 
 # Subtract the value of gift card (5.50) from the price of all items in the shopping cart (12.30) using the - operator.
-print (5.5 - 12.3)
+print (12.3 - 5.5)
