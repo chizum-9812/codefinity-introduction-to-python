9589e68d0249615e393423f1066044b6b86b1b4a
chizum-9812 <>
2026-08-13 13:21:49 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,11 @@
+
+item_name = "orange"
+item_price = 1.35
+item_stock_quantity = 99
+in_stock = True
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
