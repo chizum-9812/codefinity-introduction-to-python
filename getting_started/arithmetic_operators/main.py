92ceef3ad94a299dc1402a87639214b30d5fcbe4
chizum-9812 <>
2026-08-13 13:07:10 +0000

    Arithmetic Operators

---
+++ getting_started/arithmetic_operators/main.py
@@ -0,0 +1,5 @@
+# Add the price of milk (5.42) and the price of bread (4.39). Use + operator
+print (5.42 + 4.39)
+
+# Subtract the value of gift card (5.50) from the price of all items in the shopping cart (12.30) using the - operator.
+print (5.5 - 12.3)
