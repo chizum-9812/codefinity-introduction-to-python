c119cbc93540306e05fa8efe0f45cf59e4372464
chizum-9812 <>
2026-08-14 07:53:22 +0000

    Challenge: Inventory Sorting Capstone

---
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -0,0 +1,12 @@
+# Lists of items and categories for slicing
+items = "Bubblegum, Chocolate, Pasta"
+categories = "Candy Aisle, Pasta Aisle"
+
+candy1 = items[0:9]
+candy2 = items[11:20]
+candy3 = items[22:]
+
+category1 = categories[0:11]
+category2 = 
+
+print(candy3)
